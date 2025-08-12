456c85a490337123153ca72744835a804e6dd3fd
LTJBryant <lB@bryant-electrical.com>
2025-08-12 16:37:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const sqlite3 = require('sqlite3').verbose();
 const nodemailer = require('nodemailer');
 const path = require('path');
 const fs = require('fs');
-const { Configuration, OpenAIApi } = require('openai');
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
